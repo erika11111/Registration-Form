fb6733e253d0be1594e2a4dcb9e189c0c58bb19d
erika11111 <104774933+erika11111@users.noreply.github.com>
2023-11-19 15:18:11 +0100

    styles

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -5,6 +5,7 @@ import {
   faInfoCircle,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "./register.module.css";
 // regex statements: user regex - used for username validation
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
